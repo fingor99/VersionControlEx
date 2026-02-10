3dbb44aa9a243e839d5a5b0c747f02d998b74f4f
Finlay Gordon <159448297+fingor99@users.noreply.github.com>
2026-02-10 11:50:54 +0000

    Added heads or tails code
    Added a heads or tails gimmick that does not logically make sense, it's just for this exercise

--- Program.cs
+++ Program.cs
@@ -10,7 +10,21 @@ namespace VersionControlEx
     {
         static void Main(string[] args)
         {
+            // this is not supposed to make sense
+
             Console.WriteLine("Hello, Github!");
+            Console.WriteLine("Heads or Tails?");
+
+            string headsOrTails = Console.ReadLine();
+
+            List<string> coin = new List<string> { "You WIN", "You LOSE" };
+
+            Random coin_toss = new Random();
+
+            int index = coin_toss.Next(coin.Count);
+
+            Console.WriteLine(coin[index]);
+
         }
     }
 }
